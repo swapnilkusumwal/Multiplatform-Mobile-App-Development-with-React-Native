525e91e26623658f2f34a7aee760e0f28ce992a0
swapnilkusumwal <swapnilkusumwal@gmail.com>
2020-05-04 04:39:37 +0530

    Redux Adding Favorite

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,18 +3,27 @@ import { Text, View,ScrollView,FlatList } from 'react-native';
 import { Card,Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import {postFavorite} from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
-      comments: state.comments
+      comments: state.comments,
+      favorites: state.favorites
     }
-  }
+};
+
+const mapDispatchToProps = dispatch=>({
+    postFavorite:(dishId)=>dispatch(postFavorite(dishId))
+});
+
 function RenderDish(props) {
 
     const dish = props.dish;
     
         if (dish != null) {
+            console.log("HELLOEW");
+            console.log(props);
             return(
                 <Card
                 featuredTitle={dish.name}
@@ -68,14 +77,8 @@ function RenderComments(props){
 }
 class Dishdetail extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            favorites : []
-        };
-    }
-    markFavourite(dishId){
-        this.setState({favorites : this.state.favorites.concat(dishId)});
+    markFavorite(dishId){
+        this.props.postFavorite(dishId);
     }
     render() {
         const dishId = this.props.route.params.dishId;
@@ -83,7 +86,7 @@ class Dishdetail extends Component {
         return(
             <ScrollView>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(el => el === dishId)}
+                    favorite={this.props.favorites.some(el => el === dishId)}
                     onPress={() => this.markFavorite(dishId)} 
                     />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
@@ -91,4 +94,4 @@ class Dishdetail extends Component {
         );
     }
 }
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps,mapDispatchToProps)(Dishdetail);
