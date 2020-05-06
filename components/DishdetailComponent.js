22ea5e5bd9028928d3f65fa7b841b2b25085b12c
swapnilkusumwal <swapnilkusumwal@gmail.com>
2020-05-06 21:36:04 +0530

    Persist Redux Store

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -26,14 +26,19 @@ function RenderDish(props) {
 
     const dish = props.dish;
     
+    handleViewRef=ref=>this.view=ref;
+
     const recognizeDrag=({moveX,moveY,dx,dy})=>{
-        if(dx < -200)
+        if(dx < -100)
             return true;
         return false;
     };
     const panResponder=PanResponder.create({
         onStartShouldSetPanResponder:(e,gestureState)=>{
             return true;
+        },
+        onPanResponderGrant:()=>{this.view.rubberBand(1000)
+            .then(endState=>console.log(endState.finished? 'finished': 'cancelled'));
         },
         onPanResponderEnd:(e,gestureState)=>{
             if(recognizeDrag(gestureState))
@@ -59,7 +64,8 @@ function RenderDish(props) {
         if (dish != null) {
             return(
                 <Animatable.View animation='fadeInDown' duration ={2000} delay={1000}
-                    {...panResponder.panHandlers}>
+                    {...panResponder.panHandlers}
+                    ref={this.handleViewRef}>
                     <Card
                     featuredTitle={dish.name}
                     image={{uri: baseUrl + dish.image}}>
